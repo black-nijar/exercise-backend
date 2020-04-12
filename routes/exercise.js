6e87e1f5d08f0a574b8049ba80dbb1ffe57b0764
Nijarudeen <nijarr2020@gmail.com>
2020-04-12 18:08:50 +0530

    delete exercise

--- routes/exercise.js
+++ routes/exercise.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Exercise = require("../models/exercise");
 
+// Get exercises
 router.get("/", async (req, res) => {
   try {
-    const exercise = await Exercise.find({ exercise: req.exercise }).sort({ date: -1 });
+    const exercise = await Exercise.find({ exercise: req.exercise }).sort({
+      date: -1,
+    });
     res.json(exercise);
   } catch (error) {
     console.error(error.message);
-    res.status(400).json({ msg: error.message});
+    res.status(400).json({ msg: error.message });
   }
 });
 
+// create exercise
 router.post("/", async (req, res) => {
   try {
     const { userName, description, duration, date } = req.body;
@@ -19,14 +23,28 @@ router.post("/", async (req, res) => {
       userName,
       description,
       duration,
-      date
-    })
+      date,
+    });
     let exercise = await newExercise.save();
     res.json(exercise);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({msg: 'Server error'});
+    res.status(500).json({ msg: "Server error" });
   }
-})
+});
 
+// delete exercise
+router.delete("/:id", async (req, res) => {
+  try {
+    let exercise = await Exercise.findById(req.params.id);
+    if (!exercise) {
+      return res.status(404).json({ msg: "Exercise not found" });
+    }
+    await Exercise.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Exercise deleted..." });
+  } catch (error) {
+    console.error(error.message);
+    res.statud(500).send("Server error");
+  }
+});
 module.exports = router;
